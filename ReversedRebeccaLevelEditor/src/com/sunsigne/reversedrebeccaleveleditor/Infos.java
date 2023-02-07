659a2379e6058fd6da2d0ec9db0cff1963a689f7
Florian Murcia <florian.murcia@hotmail.fr>
2023-02-07 10:39:36 +0400

    v.a.0.1.0 theme - basic running

--- ReversedRebeccaLevelEditor/src/com/sunsigne/reversedrebeccaleveleditor/Infos.java
+++ ReversedRebeccaLevelEditor/src/com/sunsigne/reversedrebeccaleveleditor/Infos.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 public class Infos {
 
 	public static final String NAME = "Reversed Rebecca - Level Editor";
-	public static final String VERSION = "v.alpha.0.0.17";
+	public static final String VERSION = "v.alpha.0.1.0";
 	public static final boolean IS_DEV_VERSION = System.getProperty("java.class.path").contains("\\git\\");
 	public static final ImageIcon ICON = new ImageIcon("icon.png");
 
